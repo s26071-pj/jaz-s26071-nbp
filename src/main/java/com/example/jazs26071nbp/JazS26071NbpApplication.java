b4e33d63253be58dca7f0303c47c660e5b771a9c
s26071 <s26071@pjwstk.edu.pl>
2023-06-24 15:18:13 +0200

    Initial commit

---
+++ src/main/java/com/example/jazs26071nbp/JazS26071NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs26071nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS26071NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS26071NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jazs26071nbp/JazS26071NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs26071nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS26071NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
